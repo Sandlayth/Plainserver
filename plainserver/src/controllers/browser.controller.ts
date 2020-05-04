4d13eb660130e42ffb9dabe5c41301f640c11582
Sandlayth <modepadu95@riseup.net>
2020-05-05 00:05:38 +0200

    Display item type (file or directory) and file mime type

--- plainserver/src/controllers/browser.controller.ts
+++ plainserver/src/controllers/browser.controller.ts
@@ -1,15 +1,25 @@
 import {get, param} from '@loopback/rest';
 import {readdirSync} from 'fs';
+import {lookup} from 'mime-types';
 import {defaultPath} from '../config';
 import {isItemAccessible} from '../helpers/browse';
 
 export class BrowserController {
   @get('/browse')
-  hello(@param.query.string('path') path: string): Array<String> {
+  browse(@param.query.string('path') path: string): Array<Object> {
     path = defaultPath + path;
-    let items: string[] = [];
+    let items: Object[] = [];
     if (isItemAccessible(path)) {
-      items = readdirSync(path);
+      const dirItems = readdirSync(path, {withFileTypes: true});
+      items = dirItems.map(item => {
+        let type = 'directory';
+        let mime = undefined;
+        if (item.isFile()) {
+          type = 'file';
+          mime = lookup(path + item.name);
+        }
+        return {...item, type, mime};
+      })
     }
     return items;
   }
